6626a5e0940bdee7309bbaa3b19fd69953bb60b1
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-12-25 16:14:06 -0300

    chore: update config files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["image.tmdb.org"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        card: "32.8387px 24.5482px 79px rgba(0, 0, 0, 0.15), 23.8851px 17.855px 53.2479px rgba(0, 0, 0, 0.121406), 16.6246px 12.4276px 34.4391px rgba(0, 0, 0, 0.10125), 10.9035px 8.15079px 21.3701px rgba(0, 0, 0, 0.0867188), 6.56775px 4.90965px 12.8375px rgba(0, 0, 0, 0.075), 3.46346px 2.58907px 7.6377px rgba(0, 0, 0, 0.0632813), 1.4367px 1.07399px 4.56719px rgba(0, 0, 0, 0.04875), 0.333519px 0.249318px 2.42246px rgba(0, 0, 0, 0.0285938);",
+        image: "20px 20px 30px 0px #0000004D",
+      },
       colors: {
         "complementary-red": "#BA0707",
         "complementary-yellow": "#D7A82E",
