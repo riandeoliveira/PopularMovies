eb4f926916fd474f2c0f4ee200c6f9a167a63d34
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-12-27 20:56:09 -0300

    feat: add movie error messages

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,27 +1,52 @@
 import { useMovie } from "hooks/useMovie";
+import { useSelector } from "react-redux";
 import type { IMovie } from "types/movie";
+import type { RootState } from "types/redux";
 import { MovieCard } from "./MovieCard";
 
 export const MovieList = (): JSX.Element => {
   const { getSelectedMovieList } = useMovie();
+  const { filterBy } = useSelector((state: RootState) => state.movie);
 
   const movies: IMovie[] = getSelectedMovieList();
+
+  const renderMovies = (): JSX.Element | JSX.Element[] => {
+    const hasMovies: boolean = movies.length > 0;
+    const hasMovieList: boolean = filterBy === "favorites" && !hasMovies;
+    const hasFavoriteMovies: boolean = filterBy === "all" && !hasMovies;
+
+    let errorMessage: string = "";
+
+    if (!hasMovieList) {
+      errorMessage = "Nenhum filme encontrado :(";
+    }
+    if (!hasFavoriteMovies) {
+      errorMessage =
+        "Você ainda não possui filmes favoritos. Experimente adicionar alguns!";
+    }
+
+    if (hasMovies) {
+      return movies.map((movie) => (
+        <MovieCard
+          id={movie.id}
+          name={movie.name}
+          description={movie.description}
+          year={movie.year}
+          rating={movie.rating}
+          favorite={movie.favorite}
+          image={movie.image}
+          key={movie.id}
+        />
+      ));
+    }
+
+    return <p className="text-center">{errorMessage}</p>;
+  };
 
   return (
     <div className="flex justify-center pt-16 pb-16">
       <div className="flex flex-col w-[1280px] gap-12 px-4">
-        {movies.map((movie) => (
-          <MovieCard
-            id={movie.id}
-            name={movie.name}
-            description={movie.description}
-            year={movie.year}
-            rating={movie.rating}
-            favorite={movie.favorite}
-            image={movie.image}
-            key={movie.id}
-          />
-        ))}
+        {renderMovies()}
       </div>
     </div>
   );
